c922a1dcb4a49cd1d43a4c295dd8e926239670bd
Jack <907739769@qq.com>
2026-01-07 16:22:06 +0800

    调整参数设置页面

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysConfig.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysConfig.java
@@ -70,7 +70,6 @@ public class SysConfig extends BaseEntity
         this.configKey = configKey;
     }
 
-    @NotBlank(message = "参数键值不能为空")
     @Size(min = 0, max = 500, message = "参数键值长度不能超过500个字符")
     public String getConfigValue()
     {
