b9780a4a2766c19490d111e3c06493b965ededa0
Jack <907739769@qq.com>
2026-01-13 20:50:09 +0800

    调整文件监控服务线程池使用公用任务线程池

--- ruoyi-openliststrm/src/main/java/com/ruoyi/openliststrm/monitor/FileMonitorService.java
+++ ruoyi-openliststrm/src/main/java/com/ruoyi/openliststrm/monitor/FileMonitorService.java
@@ -1,9 +1,12 @@
 package com.ruoyi.openliststrm.monitor;
 
+import com.ruoyi.common.utils.Threads;
+import com.ruoyi.framework.manager.AsyncManager;
 import com.ruoyi.openliststrm.processor.FileProcessor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.file.Path;
+import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -17,8 +20,6 @@ public class FileMonitorService {
 
     private final FileMonitor monitor;
     private final FileProcessor processor;
-    private final ScheduledExecutorService executor =
-            Executors.newScheduledThreadPool(10);
 
     public FileMonitorService(FileMonitor monitor, FileProcessor processor) {
         this.monitor = monitor;
@@ -28,14 +29,18 @@ public class FileMonitorService {
     public void start() {
         monitor.setListener(event -> {
             Path p = event.getPath();
-            executor.schedule(() -> processor.process(p), 2, TimeUnit.SECONDS);
+            AsyncManager.me().execute(new TimerTask() {
+                @Override
+                public void run() {
+                    processor.process(p);
+                }
+            });
         });
         monitor.start();
     }
 
     public void stop() {
         monitor.stop();
-        executor.shutdown();
     }
 }
 
