a9c3eef2bd0b2ef3dc09698ee1a044f025e717d9
Jack <907739769@qq.com>
2026-05-13 00:13:17 +0800

    refactor: 迁移 MyBatis-Plus 至 3.5.16 并移除 Wrappers 依赖
    - 升级 mybatis-plus-spring-boot3-starter 为 spring-boot4-starter (3.5.16)
    - 替换 mybatis-plus-generator 为 mybatis-plus-jsqlparser
    - CopyHelper/StrmHelper 移除 Wrappers，改用显式 lambdaQuery + updateById

--- ruoyi-openliststrm/src/main/java/com/ruoyi/openliststrm/helper/CopyHelper.java
+++ ruoyi-openliststrm/src/main/java/com/ruoyi/openliststrm/helper/CopyHelper.java
@@ -1,12 +1,16 @@
 package com.ruoyi.openliststrm.helper;
 
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.ruoyi.common.utils.bean.BeanUtils;
 import com.ruoyi.framework.manager.AsyncManager;
 import com.ruoyi.openliststrm.mybatisplus.domain.OpenlistCopyPlus;
 import com.ruoyi.openliststrm.mybatisplus.service.IOpenlistCopyPlusService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
 /**
  * @Author Jack
  * @Date 2025/7/17 11:07
@@ -25,12 +29,19 @@ public class CopyHelper {
                 //加锁 简单解决并发情况插入重复数据
                 synchronized (LOCK) {
                     //保存或者更新
-                    openlistCopyPlusService.saveOrUpdate(openlistCopyPlus, Wrappers.<OpenlistCopyPlus>lambdaUpdate()
-                            .eq(StringUtils.isNotBlank(openlistCopyPlus.getCopySrcPath()), OpenlistCopyPlus::getCopySrcPath, openlistCopyPlus.getCopySrcPath())
-                            .eq(StringUtils.isNotBlank(openlistCopyPlus.getCopyDstPath()), OpenlistCopyPlus::getCopyDstPath, openlistCopyPlus.getCopyDstPath())
-                            .eq(StringUtils.isNotBlank(openlistCopyPlus.getCopySrcFileName()), OpenlistCopyPlus::getCopySrcFileName, openlistCopyPlus.getCopySrcFileName())
-                            .eq(StringUtils.isNotBlank(openlistCopyPlus.getCopyDstFileName()), OpenlistCopyPlus::getCopyDstFileName, openlistCopyPlus.getCopyDstFileName())
-                    );
+                    List<OpenlistCopyPlus> copyList = openlistCopyPlusService.lambdaQuery()
+                            .eq(OpenlistCopyPlus::getCopySrcPath,openlistCopyPlus.getCopySrcPath())
+                            .eq(OpenlistCopyPlus::getCopySrcFileName,openlistCopyPlus.getCopySrcFileName())
+                            .list();
+                    if (!CollectionUtils.isEmpty(copyList)) {
+                        OpenlistCopyPlus newCopy = copyList.get(0);
+                        int id = newCopy.getCopyId();
+                        BeanUtils.copyProperties(openlistCopyPlus, newCopy);
+                        newCopy.setCopyId(id);
+                        openlistCopyPlusService.updateById(newCopy);
+                    } else {
+                        openlistCopyPlusService.save(openlistCopyPlus);
+                    }
                 }
             });
     }
