b7279a4eb4ba5d27dc74cd07f750fb3346a49280
Jack <907739769@qq.com>
2026-07-20 20:18:31 +0800

    feat: 新增 RenameCategoryRulePlusMapper

---
+++ ruoyi-openliststrm/src/main/java/com/ruoyi/openliststrm/mybatisplus/mapper/RenameCategoryRulePlusMapper.java
@@ -0,0 +1,16 @@
+package com.ruoyi.openliststrm.mybatisplus.mapper;
+
+import com.ruoyi.openliststrm.mybatisplus.domain.RenameCategoryRulePlus;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 重命名分类规则配置 Mapper 接口
+ * </p>
+ *
+ * @author Jack
+ * @since 2026-07-20
+ */
+public interface RenameCategoryRulePlusMapper extends BaseMapper<RenameCategoryRulePlus> {
+
+}
